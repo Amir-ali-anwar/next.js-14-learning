252dfeaa8eaa1b972a425b34d4f163279a39a1ec
Amir Ali Anwar <amiralianwar611@gmail.com>
2024-01-06 00:00:44 +0500

    adding pages

--- app/page.js
+++ app/page.js
@@ -5,7 +5,12 @@ const page = () => {
     <>
     <div>Home page</div>
     <Link href="/about" style={{display:'block'}}>About page</Link>
-    <Link href="/about/info">Info page</Link>
+    <Link href="/about/info" style={{display:'block'}}>Info page</Link>
+    <Link href="/about/info" style={{display:'block'}}>Info page</Link>
+    <Link href="/query" style={{display:'block'}}>query page</Link>
+    <Link href="/task" style={{display:'block'}}>task page</Link>
+    <Link href="/prisma-example" style={{display:'block'}}>prisma-example</Link>
+    <Link href="/drinks" style={{display:'block'}}>drinks</Link>
     </>
   )
 }
