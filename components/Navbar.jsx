4e72ccf72e9d534608cb6ded4fab9ee232a7b898
Amir Ali Anwar <amiralianwar611@gmail.com>
2024-01-06 11:38:54 +0500

    understanding the next.js

---
+++ components/Navbar.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+const links = [
+  { href: "/client", label: "client" },
+  { href: "/drinks", label: "drinks" },
+  { href: "/prisma-example", label: "prisma" },
+  { href: "/tasks", label: "tasks" },
+];
+
+const Navbar = () => {
+  return <nav className="bg-base-300 py-4">fsdgsfg</nav>;
+};
+
+export default Navbar;
