5a844cfe43dd6a54d8f7ea32e43784022d179a65
Amir Ali Anwar <amiralianwar611@gmail.com>
2024-01-06 19:01:10 +0500

    understanding the next.js concepts

--- app/drinks/page.js
+++ app/drinks/page.js
@@ -1,26 +1,25 @@
 import React from 'react'
+import DrinksList from '@/components/DrinksList'
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a';
 const fetchDrinks = async () => {
- try {
-  await new Promise((resolve) => setTimeout(resolve, 2000))
-  const response = await fetch(url)
-  if(!response.ok){
-    throw new Error("Failed to fetch Drinks")
-  }
-  const data = await response.json()
-  return data
- } catch (error) {
+  try {
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+    const response = await fetch(url)
+    if (!response.ok) {
+      throw new Error("Failed to fetch Drinks")
+    }
+    const data = await response.json()
+    return data
+  } catch (error) {
     console.log(error);
- }
+  }
 }
 const page = async () => {
-  const data= await fetchDrinks()
-  console.log(data);
+  const data = await fetchDrinks()
   return (
-    <>
-      <div>Drinks page</div>
-
-    </>
+    <div>
+      <DrinksList drinks={data?.drinks} />
+    </div>
   )
 }
 
