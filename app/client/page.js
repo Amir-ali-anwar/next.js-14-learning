6a70f9b3cf58a8293c5b20882426751ec1a1033d
Amir Ali Anwar <amiralianwar611@gmail.com>
2024-01-06 15:03:54 +0500

    next learning

--- app/client/page.js
+++ app/client/page.js
@@ -1,10 +1,14 @@
-import React from 'react'
-import Link from 'next/link'
+'use client'
+import React, { useState } from 'react'
 const page = () => {
+  const [counter, setCounter] = useState(0)
   return (
     <>
-    <div>Client page</div>
-   
+      <div>
+        <h1 className='text-7xl font-bold mb-4'>{counter}</h1>
+        <button className='btn btn-primary' onClick={()=>setCounter(counter + 1)}>increase</button>
+      </div>
+
     </>
   )
 }
--- app/drinks/page.js
+++ app/drinks/page.js
@@ -1,10 +1,25 @@
 import React from 'react'
-import Link from 'next/link'
-const page = () => {
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a';
+const fetchDrinks = async () => {
+ try {
+  await new Promise((resolve) => setTimeout(resolve, 2000))
+  const response = await fetch(url)
+  if(!response.ok){
+    throw new Error("Failed to fetch Drinks")
+  }
+  const data = await response.json()
+  return data
+ } catch (error) {
+    console.log(error);
+ }
+}
+const page = async () => {
+  const data= await fetchDrinks()
+  console.log(data);
   return (
     <>
-    <div>Drinks page</div>
-   
+      <div>Drinks page</div>
+
     </>
   )
 }
