6fca329b1a936aadb192a810dfdebd4462dcb7c7
Amir Ali Anwar <amiralianwar611@gmail.com>
2024-01-07 10:45:51 +0500

    edit task

--- components/TaskEditForm.jsx
+++ components/TaskEditForm.jsx
@@ -1,37 +1,54 @@
-import React from 'react'
-const editTask=async (formData)=>{
+import { revalidatePath } from "next/cache";
+import { redirect } from 'next/navigation';
+import React from "react";
+const editTask = async (formData) => {
   "use server";
-  const id = formData.get('id');
-  const content = formData.get('content');
-  const completed = formData.get('completed');
-
-}
-const TaskEditForm = ({task}) => {
-  const { id, completed, content}= task
+  const id = formData.get("id");
+  const content = formData.get("content");
+  const completed = formData.get("completed");
+  await prisma.task.update({
+    where: {
+      id,
+    },
+    data: {
+      content,
+      completed: completed === "on" ? true : false,
+    },
+  });
+  redirect('/task')
+};
+const TaskEditForm = ({ task }) => {
+  const { id, completed, content } = task;
   return (
-    <form className='max-w-sm p-12 border border-base-300 rounded-lg' action={editTask}>
-       <input type='hidden' name='id' value={id} />
+    <form
+      className="max-w-sm p-12 border border-base-300 rounded-lg"
+      action={editTask}
+    >
+      <input type="hidden" name="id" value={id} />
       <input
-        type='text'
+        type="text"
         required
         defaultValue={content}
-        name='content'
-        className='input input-bordered w-full'
+        name="content"
+        className="input input-bordered w-full"
       />
-      <div className='form-control my-4'>
-        <label htmlFor='completed' className='label cursor-pointer'>
-          <span className='label-text'>completed</span>
+      <div className="form-control my-4">
+        <label htmlFor="completed" className="label cursor-pointer">
+          <span className="label-text">completed</span>
           <input
-            type='checkbox'
+            type="checkbox"
             defaultChecked={completed}
-            id='completed'
-            name='completed'
-            className='checkbox checkbox-primary checkbox-sm'
+            id="completed"
+            name="completed"
+            className="checkbox checkbox-primary checkbox-sm"
           />
         </label>
       </div>
+      <button type='submit' className='btn btn-primary btn-block btn-sm'>
+        edit
+      </button>
     </form>
-  )
-}
+  );
+};
 
-export default TaskEditForm
+export default TaskEditForm;
